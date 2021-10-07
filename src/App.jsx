5d341f13b9fdc7ee6e520de95fa8ee0a76c610be
jdkumawat <jagdish@fastcoo.com>
2021-10-07 12:27:32 +0530

    set all

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import { useState, useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-
 function App() {
   useEffect(() => {
     AOS.init({
--- src/data.js
+++ src/data.js
@@ -54,4 +54,3 @@ export const webBranding = [
     img: "assets/top11.jpg",
   },
 ];
-
